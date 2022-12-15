e1efde06fbb2e888ff60295cd075942e0a557631
seanharrington256 <seanharrington256@gmail.com>
2022-12-15 14:50:38 -0700

    add heatmap gdm

--- GDM_coexp.R
+++ GDM_coexp.R
@@ -6,6 +6,8 @@ library(adegenet)
 library(gdm)
 library(caret)
 library(vcfR)
+library(reshape2)
+library(ggplot2)
 
 ## Set up an object to contain the path to the main directory with the data and then set that as the working directory
 main_dir<-"/Users/harrington/Active_Research/Ecotone_genomics/GBS_Data/pop_assignment"
@@ -13,12 +15,20 @@ setwd(main_dir)
 coords<-read.csv("all_coords_requested.csv", header=TRUE, row.names=NULL) # coordinates of everything I sequenced and many I didn't
 
 
-
 ## make a directory to put the output into
 gdm_out_dir<-paste0(main_dir, "/Coexp_gdm_out")  # specify a full path to the directory
 if(!dir.exists(gdm_out_dir)){ # check if the directory  exists and then only create it if it does not
   dir.create(gdm_out_dir)
 }
+
+
+## Read in environmental data (this was extracted from Bioclim variables in script "IBD_coexp.R")
+all_alt<-read.csv("Coexp_coords_altitude.csv", stringsAsFactors = FALSE, row.names=1)
+all_cur_bioclim_only<-read.csv("Coexp_coords_present_bioclim.csv", stringsAsFactors = FALSE, row.names=1)
+all_cur_envirem<-read.csv("Coexp_coords_present_envirem.csv", stringsAsFactors = FALSE, row.names=1)
+all_cur_bioclim<-cbind(all_cur_bioclim_only, all_cur_envirem) ## note that despite the name, this also includes Envirems
+
+
 
 
 
@@ -29,14 +39,14 @@ if(!dir.exists(gdm_out_dir)){ # check if the directory  exists and then only cre
 ######################################################################################################################
 
 assemblies_gdm<-c(
-  # "Acontortrix_p123_v2_25miss",
-  # "Dpunctatus_p123_v3_25missEAST",
-  # "Lgetula_p123_v4_25miss", # note that PTA & Stairway was made from Lgetula_p123_v2_25miss
-  # "Pguttatus_p123_v2_25miss",
-  # "Sdekayi_p123_v2_25miss",
-  # "erytro",
-  # "abacura_only",
-  # "Mflagellum_p123_v3_25missEast",
+  "Acontortrix_p123_v2_25miss",
+  "Dpunctatus_p123_v3_25missEAST",
+  "Lgetula_p123_v4_25miss", # note that PTA & Stairway was made from Lgetula_p123_v2_25miss
+  "Pguttatus_p123_v2_25miss",
+  "Sdekayi_p123_v2_25miss",
+  "erytro",
+  "abacura_only",
+  "Mflagellum_p123_v3_25missEast",
   "Milks_filtered_snps_taxa"
 )
 
@@ -74,15 +84,8 @@ for(species in assemblies_gdm){   ### if we want to loop over all species, this 
   Dgen<-dist(gendata, diag=TRUE, upper=TRUE)
   gdm_dgen<-Dgen/max(Dgen) #normalize the genetic distances
   
-  ## Read in environmental data (this was extracted from Bioclim variables in script "IBD_coexp.R")
-  all_alt<-read.csv("Coexp_coords_altitude.csv", stringsAsFactors = FALSE, row.names=1)
-  all_cur_bioclim_only<-read.csv("Coexp_coords_present_bioclim.csv", stringsAsFactors = FALSE, row.names=1)
-  all_cur_envirem<-read.csv("Coexp_coords_present_envirem.csv", stringsAsFactors = FALSE, row.names=1)
-  all_cur_bioclim<-cbind(all_cur_bioclim_only, all_cur_envirem) ## note that despite the name, this also includes Envirems
   
-  
-  
-  # match up these data to the order of the individuals in genetic data (also prunes these data to just those for the assembly of interest)
+  # match up environmental data to the order of the individuals in genetic data (also prunes these data to just those for the assembly of interest)
   match_bioclim<-match(ind_names, rownames(all_cur_bioclim))
   bioclim<-all_cur_bioclim[match_bioclim,]
   match_alt<-match(ind_names, rownames(all_alt))
@@ -126,7 +129,7 @@ for(species in assemblies_gdm){   ### if we want to loop over all species, this 
   gdm_sitepair<-formatsitepair(gdm_dgen_named, bioFormat=3, XColumn="lon", YColumn="lat", predData=env_preds, siteColumn="ind_names")
 
   # use this to run gdm
-  gdm.1 <- gdm(gdm_sitepair, geo=T)
+  gdm.1 <- gdm(gdm_sitepair, geo=TRUE)
   
   # str(gdm.1)
   # summary(gdm.1)
@@ -156,5 +159,125 @@ for(species in assemblies_gdm){   ### if we want to loop over all species, this 
   setwd(main_dir)
   
 }
+
+
+####################################################################################################
+#### Some summaries across species:
+####################################################################################################
+
+# Read in the csv files that tell if each model is significant
+model_sig_files <- list.files(gdm_out_dir, pattern = "GDM_p-value.csv", full.names = TRUE)
+model_sig <- lapply(model_sig_files, read.csv)
+tax_names <- sapply(model_sig_files, function(x) strsplit(basename(x), "_GDM_")[[1]][1])
+names(model_sig) <- tax_names
+
+# Which are significant for the whole model?? - model p-value is row 3 in all, value is column 2
+model_sig_values_only <- sapply(model_sig, function(x) x[3,2])
+sig_models <- model_sig_values_only[model_sig_values_only < 0.05]
+spec_sig_model <- names(sig_models) # species that are significant in the full model
+
+
+# For each of these, find out which predictors are significant
+var_sig_files <- list.files(gdm_out_dir, pattern = "GDM_var_sig.csv", full.names = TRUE)
+var_sig <- lapply(var_sig_files, read.csv)
+tax_names <- sapply(var_sig_files, function(x) strsplit(basename(x), "_GDM_")[[1]][1])
+names(var_sig) <- tax_names
+
+
+# if the full model isn't significant, I don't think there's any case where an individual variable should be called significant, but let's make sure
+#     the opposite seems the be the case, it's possible for the full model to be significant even if no ineividual variable is
+var_sig <- var_sig[names(var_sig) %in% spec_sig_model]
+
+
+# Get out the names of predictors that are significant at < 0.05 for each species
+sig_vars <- lapply(var_sig, function(x) x[x$All.predictors < 0.05, 1])
+
+
+# Go in and pull out the variable importance for significant models, let's plot this as heatmaps
+#     do it 2 ways - only include variables if they're significant for an assembly, or include all importances
+#     for any species if the full model is significant
+
+# get variable importance
+var_imp_files <- list.files(gdm_out_dir, pattern = "GDM_var_imp.csv", full.names = TRUE)
+var_imp <- lapply(var_imp_files, read.csv)
+tax_names <- sapply(var_imp_files, function(x) strsplit(basename(x), "_GDM_")[[1]][1])
+names(var_imp) <- tax_names
+
+# If the full model is not significant, replace importances with NA
+non_sig_models <- names(var_imp)[!names(var_imp) %in% spec_sig_model]
+if(length(non_sig_models) > 0){
+  for(i in 1:length(non_sig_models)){
+    var_imp[[non_sig_models[i]]][,2] <- NA
+  }
+}
+
+
+
+# fix up some names
+names(var_imp) <- gsub("abacura_only", "F. abacura", names(var_imp))
+names(var_imp) <- gsub("Acontortrix_p123_v2_25miss", "A. contortrix", names(var_imp))
+names(var_imp) <- gsub("Dpunctatus_p123_v3_25missEAST", "D. punctatus", names(var_imp))
+names(var_imp) <- gsub("erytro", "F. erytrogramma", names(var_imp))
+names(var_imp) <- gsub("Lgetula_p123_v4_25miss", "L. getula", names(var_imp))
+names(var_imp) <- gsub("Mflagellum_p123_v3_25missEast", "M. flagellum", names(var_imp))
+names(var_imp) <- gsub("Milks_filtered_snps_taxa", "L. triangulum", names(var_imp))
+names(var_imp) <- gsub("Pguttatus_p123_v2_25miss", "P. guttatus", names(var_imp))
+names(var_imp) <- gsub("Sdekayi_p123_v2_25miss", "S. dekayi", names(var_imp))
+# and for sig_vars
+names(sig_vars) <- gsub("abacura_only", "F. abacura", names(sig_vars))
+names(sig_vars) <- gsub("Acontortrix_p123_v2_25miss", "A. contortrix", names(sig_vars))
+names(sig_vars) <- gsub("Dpunctatus_p123_v3_25missEAST", "D. punctatus", names(sig_vars))
+names(sig_vars) <- gsub("erytro", "F. erytrogramma", names(sig_vars))
+names(sig_vars) <- gsub("Lgetula_p123_v4_25miss", "L. getula", names(sig_vars))
+names(sig_vars) <- gsub("Mflagellum_p123_v3_25missEast", "M. flagellum", names(sig_vars))
+names(sig_vars) <- gsub("Milks_filtered_snps_taxa", "L. triangulum", names(sig_vars))
+names(sig_vars) <- gsub("Pguttatus_p123_v2_25miss", "P. guttatus", names(sig_vars))
+names(sig_vars) <- gsub("Sdekayi_p123_v2_25miss", "S. dekayi", names(sig_vars))
+
+
+
+
+
+# If a variable is not significant for a given taxon, also make that importance NA
+var_imp_sig_only <- var_imp
+for(i in 1:length(sig_vars)){
+  non_sig_vars <- !var_imp_sig_only[[names(sig_vars)[i]]][,1] %in% sig_vars[[i]]  # find the predictors that are not in the list of sig variables for each taxon
+  var_imp_sig_only[[names(sig_vars)[i]]][non_sig_vars,2] <- NA # replace the importances for those with NA
+}
+
+
+### Make heatmaps of each of these - either only sig variables, or all sig variables for any model that is significant overall
+
+# Formatting into a single dataframe each
+# melt the list into a dataframe and ditch a column we don't need
+var_imp_df <- melt(var_imp, id = "X")
+var_imp_df <- var_imp_df[, !colnames(var_imp_df) %in% "variable"]
+colnames(var_imp_df) <- c("variable", "importance", "species")
+
+
+var_imp_sig_only_df <- melt(var_imp_sig_only, id = "X")
+var_imp_sig_only_df <- var_imp_sig_only_df[, !colnames(var_imp_sig_only_df) %in% "variable"]
+colnames(var_imp_sig_only_df) <- c("variable", "importance", "species")
+
+
+# make heatmap with ggplot
+ggplot(var_imp_df, aes(species, variable, fill= importance, na.rm = TRUE)) + 
+  geom_tile(na.rm = TRUE) +
+  scale_fill_gradient2(mid = "white",  high = "red", na.value = NA) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+        axis.title.x = element_blank(), axis.title.y = element_blank())
+
+# make heatmap with ggplot for significant only
+ggplot(var_imp_sig_only_df, aes(species, variable, fill= importance, na.rm = TRUE)) + 
+  geom_tile(na.rm = TRUE) +
+  scale_fill_gradient2(mid = "white",  high = "red", na.value = NA) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+        axis.title.x = element_blank(), axis.title.y = element_blank())
+
+
+
+
+
+
 
 
