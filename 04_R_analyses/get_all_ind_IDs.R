36439c603be9ef03d3671441f841665e681ca10b
seanharrington256 <seanharrington256@gmail.com>
2023-12-15 15:39:59 -0700

    add yarn plot, Dpunctatus ID fix, appendix

--- 04_R_analyses/get_all_ind_IDs.R
+++ 04_R_analyses/get_all_ind_IDs.R
@@ -1,4 +1,5 @@
 #### Script to do get all the indvidual IDs used in the paper
+# and make a table of populations
 
 
 # had to change YPM13949 in the coords to YP13949 to match the genetic data files
@@ -26,6 +27,10 @@ all_assemblies<-c(
   "Mflagellum_p123_v3_25missEast"
 )
 
+
+
+
+
 #### Some overall setup for mapping and plotting
 
 
@@ -52,6 +57,41 @@ for(i in seq_along(all_assemblies)){
   ind_names[[i]] <- gendata@ind.names ## get the individual names
 }
 
-
+# write out the individuals
 all_inds <- unlist(ind_names)
 write.table(all_inds, file = "all_inds_coexp.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+
+
+##### After doing this, and then some stuff to grab the number of reads per individual
+##   read that back in with other info to be combined into an appendix
+
+all_inds <- read.table("all_inds_coexp.txt") # all the newly sequenced indiviudals
+colnames(all_inds) <- "Sample_ID"
+num_reads <- read.table("num_forw_reads_per_samp.txt")  # reads for individuals other than Farancia
+colnames(num_reads) <- c("Sample_ID", "Reads")
+
+# Merge these together:
+inds_reads <- merge(all_inds, num_reads, all = TRUE)
+
+# go to the directory with Stairwayplot population files and use these to fill in some extra info
+setwd("~/Active_Research/Ecotone_genomics/GBS_Data/Coexp_dryad/stairwayplot2/pop_files")
+
+# Read in each file
+popfiles <- list.files(pattern = ".txt")
+popinfo <- list()
+
+for(i in seq_along(popfiles)){
+  popinfo[[i]] <- read.table(popfiles[[i]]) # get the pop info 
+  popinfo[[i]]$Species <- strsplit(popfiles[[i]], split = "_")[[1]][[1]] # make a column of just the species name
+}
+
+popinfo_df <- do.call(rbind, popinfo)
+colnames(popinfo_df) <- c("Sample_ID", "Population", "Species_or_complex")
+
+# merge that with inds_reads
+appendix_info <- merge(inds_reads, popinfo_df, all.x = TRUE)
+
+# Do a little renaming:
+appendix_info$Species_or_complex <- gsub("abacura", "Fabacura", appendix_info$Species_or_complex)
+appendix_info$Species_or_complex <- gsub("erytro", "Ferytrogramma", appendix_info$Species_or_complex)
